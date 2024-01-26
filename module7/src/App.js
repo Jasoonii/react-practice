6b7253b3c4e8711ce6cb3d29acb333e5801564ab
Jasoonii <jason.ddang1@gmail.com>
2024-01-26 16:24:48 -0600

    Following Module 7 Tutorial

--- module7/src/App.js
+++ module7/src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
 import Axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  Axios.get("https://catfact.ninja/fact").then((res) => {
-    console.log(res.data);
-  })
+
+  //BASIC FETCH FROM API AND CONSOLE LOG DATA
+  /*fetch('https://reqres.in/api/users')
+    .then(res => res.json())
+    .then(data => console.log(data))*/
+
+  const [catFact, setCatFact] = useState("");
+
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact);
+    });
+  }
+
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
+  
+
   return (
     <div className="App">
-      <button>Generate Cat Fact</button>
-      <p></p>
+      <button onClick={fetchCatFact}>Generate Cat Fact</button>
+      <p>{catFact}</p>
     </div>
   );
 }
