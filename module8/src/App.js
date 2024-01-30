e09924727a7982722228d27a2819f87ca1a2ed1e
Jasoonii <jason.ddang1@gmail.com>
2024-01-30 16:27:31 -0600

    Using Links to make navbar

--- module8/src/App.js
+++ module8/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { Home } from "./pages/Home"
+import { Menu } from "./pages/Menu"
+import { Contact } from "./pages/Contact"
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <div>
+          <Link to="/"> Home </Link>
+          <Link to="/menu"> Menu </Link>
+          <Link to="/contact"> Contact </Link>
+        </div>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={ <h1> PAGE NOT FOUND </h1> } />
         </Routes>
       </Router>
     </div>
