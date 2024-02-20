e77ccb42e10bf325712f028c3c4e1da7a83e4224
Jasoonii <jason.ddang1@gmail.com>
2024-02-20 01:56:04 -0600

    Practicing Module 10

--- module9/src/App.js
+++ module9/src/App.js
@@ -5,25 +5,29 @@ import { Profile } from "./pages/Profile"
 import { Contact } from "./pages/Contact"
 import { Navbar } from "./Navbar"
 import { useState, createContext } from "react"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export const AppContext = createContext();
 
 function App() {
+  const client = new QueryClient();
   const [username, setUsername] = useState("Jasooni");
 
   return (
     <div className="App">
-      <AppContext.Provider value={{ username, setUsername }}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={ <h1> PAGE NOT FOUND </h1> } />
-          </Routes>
-        </Router>
-      </AppContext.Provider>
+      <QueryClientProvider client={client}>
+        <AppContext.Provider value={{ username, setUsername }}>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={ <h1> PAGE NOT FOUND </h1> } />
+            </Routes>
+          </Router>
+        </AppContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 }
--- module9/src/pages/Home.js
+++ module9/src/pages/Home.js
@@ -1,6 +1,20 @@
 import { useContext } from "react";
 import { AppContext } from "../App";
+import { useQuery } from "@tanstack/react-query";
+import Axios from "axios";
+
 export const Home = (props) => {
+    const { data } = useQuery({
+        queryKey: ["cat"],
+        queryFn: () => Axios.get("https://catfact.ninja/fact").then((res) => res.data)
+    });
+
     const {username} = useContext(AppContext);
-    return <h1>HOME PAGE user: {username} </h1>
+    
+    return (
+        <h1>HOME PAGE user: {username} 
+            <p>{data.fact}</p>
+        </h1>
+    )
+    
 };
