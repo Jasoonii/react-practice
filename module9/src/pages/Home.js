189cf715b5c1e31720b2abe1cd5a0c1f67df3096
Jasoonii <jason.ddang1@gmail.com>
2024-02-29 19:52:14 -0600

    Finished Module 10, in module9 folder

--- module9/src/pages/Home.js
+++ module9/src/pages/Home.js
@@ -4,16 +4,26 @@ import { useQuery } from "@tanstack/react-query";
 import Axios from "axios";
 
 export const Home = (props) => {
-    const { data } = useQuery({
+    const { data: catData, isLoading, isError, refetch } = useQuery({
         queryKey: ["cat"],
         queryFn: () => Axios.get("https://catfact.ninja/fact").then((res) => res.data)
     });
 
     const {username} = useContext(AppContext);
+
+    if (isLoading) {
+        return <h1> Loading fact... </h1>
+    }
+
+    if (isError) {
+        return <h1> Error...</h1>
+    }
     
     return (
         <h1>HOME PAGE user: {username} 
-            <p>{data.fact}</p>
+            <p>{catData?.fact}</p>
+
+            <button onClick={refetch}>New Fact!</button>
         </h1>
     )
     
