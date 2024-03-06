bee1d91002005c2e08825621ebc92571dfcaa541
Jasoonii <jason.ddang1@gmail.com>
2024-03-06 00:21:42 -0600

    Created Forms with requirements

---
+++ module11/src/components/Form.js
@@ -0,0 +1,29 @@
+import { useForm } from "react-hook-form"
+import * as yup from "yup"
+
+export const Form = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
+    const schema = yup.object().shape({
+        fullName: yup.string().required(),
+        email: yup.string().email().required(),
+        age: yup.number().positive().integer().min(18).required(),
+        password: yup.string().min(4).max(20).required(),
+        confirmPassword: yup.string().oneOf([yup.ref("password"), null]).required()
+    })
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder="Full Name" {...register("fullName")} />
+            <input type="text" placeholder="Email" {...register("email")} />
+            <input type="number" placeholder="Age" {...register("age")} />
+            <input type="password" placeholder="Password" {...register("password")} />
+            <input type="password" placeholder="Confirm Password" {...register("confirmPassword")} />
+            <input type="submit" />
+        </form>
+    )
+}
